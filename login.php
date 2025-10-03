4304fceb3a248a04ecff823a815404783dfade34
reasonsx <kamkre01@easv365.dk>
2025-10-03 15:39:11 +0200

    fix

--- login.php
+++ login.php
@@ -8,9 +8,9 @@
 
 <?php include 'header.php'; ?>
 
-<section class="flex justify-center items-center min-h-[80vh] bg-primary px-4">
+<section class="flex justify-center items-center min-h-[80vh] bg-light px-4">
     <div class="bg-white rounded-2xl shadow-lg p-8 w-full max-w-md">
-        <h2 class="text-4xl font-header mb-6 text-center text-primary">Log in</h2>
+        <h2 class="text-3xl font-header mb-6 text-center text-primary">Log in</h2>
         <form action="login_process.php" method="POST" class="flex flex-col gap-4">
 
             <!-- Email -->
