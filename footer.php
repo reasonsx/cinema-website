4527523f7754d8f59adad0d0476f69076c1a3cf2
reasonsx <kamkre01@easv365.dk>
2025-10-03 10:48:01 +0200

    fix

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-black text-white py-6 text-center">
+    <p>&copy; <?php echo date("Y"); ?> MyCinema. All rights reserved.</p>
+</footer>
--- index.php
+++ index.php
@@ -74,9 +74,7 @@
 </section>
 
 <!-- Footer -->
-<footer class="bg-black text-white py-6 text-center">
-    <p>&copy; <?php echo date("Y"); ?> MyCinema. All rights reserved.</p>
-</footer>
+<?php include 'footer.php'; ?>
 
 </body>
 </html>
