89b11688d7db2cfadb932d12fb438c9fecdeb4d4
nakaaay13 <karbrz01@easv365.dk>
2025-11-01 18:17:37 +0100

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
+require_once '../include/connection.php';
+
+if (isset($_SESSION['user_id'])) {
+    $stmt = $db->prepare("UPDATE users SET remember_token = NULL WHERE id = ?");
+    $stmt->execute([$_SESSION['user_id']]);
+}
+
 session_unset();
 session_destroy();
-header('Location: login.php');
+
+setcookie('remember_token', '', time() - 3600, '/', '', isset($_SERVER['HTTPS']), true);
+
+header("Location: /cinema-website/auth/login.php");
 exit;
-?>
