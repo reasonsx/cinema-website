e2f841f20445df2bf1232b05b0cbae883ed605f5
nakaaay13 <karbrz01@easv365.dk>
2025-11-17 18:31:02 +0100

    stripe

--- stripe_config.php
+++ stripe_config.php
@@ -2,4 +2,4 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Set your Stripe secret key (Test mode)
-\Stripe\Stripe::setApiKey('sk_test_51SUVxVFU6pbKYvYW60oRfLOZYqUvzrwIvzVpLEQp9QPKGW4IjXJgwgZEwqwn04eMDHOArdlGuKYXna7Y3vwosoJM00tYwEcCzl');
+\Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
