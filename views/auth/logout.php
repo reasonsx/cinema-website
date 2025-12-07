50af3fb384c23734e0f64cded4d452b0714775d4
Tymon Mikula <karbrz01@easv365.dk>
2025-12-07 13:10:08 +0100

    Update logout.php

--- views/auth/logout.php
+++ views/auth/logout.php
@@ -1,9 +1,9 @@
 <?php
 require_once '../../backend/connection.php';
-require_once '../../auth/session.php';
+require_once 'session.php';
 
 $session = new SessionManager($db);
 $session->logout();
 
-header("Location: /cinema-website/auth/login.php");
+header("Location: /cinema-website/views/auth/login.php");
 exit;
