cfff423346e3d171df59f5806d5428bf069badbb
Tymon Mikula <karbrz01@easv365.dk>
2025-12-07 13:30:56 +0100

    Update book.php

--- views/booking/book.php
+++ views/booking/book.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../backend/connection.php';
-require_once __DIR__ . '/../../auth/session.php'; // SessionManager class
+require_once __DIR__ . '/../../views/auth/session.php'; // SessionManager class
 require_once __DIR__ . '/../../admin_dashboard/views/screenings/screenings_functions.php';
 require_once __DIR__ . '/../../admin_dashboard/views/screening_rooms/screening_rooms_functions.php';
 
