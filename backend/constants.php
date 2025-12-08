d76d3648c57156228957cf22c1657b52ed1a74a5
reasonsx <kamkre01@easv365.dk>
2025-12-08 18:55:23 +0100

    Update constants.php

--- backend/constants.php
+++ backend/constants.php
@@ -4,15 +4,12 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-// Load .env variables
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-// Define constants from .env
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_NAME', $_ENV['DB_NAME']);
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASS', $_ENV['DB_PASS']);
 
-// Build DSN dynamically
 define('DSN', "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8");
